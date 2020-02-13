44c7388d7a54a1264d9df64d21caa253c6a5f4ff
Nareg Karamardian <nareg.Karamardian@gmail.com>
2020-02-12 22:02:13 -0500

    Updates

---
+++ precache-manifest.91baaf5c424e2d4106dcb5e218b0edf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281ea06b4a4f372ba40485e3817e3ba8",
+    "url": "/Monstres-Rolodex/index.html"
+  },
+  {
+    "revision": "c54a49666daa70e5b791",
+    "url": "/Monstres-Rolodex/static/css/main.07b34f54.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/Monstres-Rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Monstres-Rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54a49666daa70e5b791",
+    "url": "/Monstres-Rolodex/static/js/main.6dcabdbe.chunk.js"
+  },
+  {
+    "revision": "a702d18edbfaa28224bb",
+    "url": "/Monstres-Rolodex/static/js/runtime-main.c5a0d884.js"
+  }
+]);
